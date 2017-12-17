72716ce4b87997a7aaed9fb1fc100e87ed360c88
SwapnaNadendla <31910494+SwapnaNadendla@users.noreply.github.com>
2017-12-17 20:51:05 +0530

    commit

--- app/src/main/java/com/example/user/myapplication/GoogleMapsActivity.java
+++ app/src/main/java/com/example/user/myapplication/GoogleMapsActivity.java
@@ -33,17 +33,6 @@ public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCa
         lng=bundle.getDouble("LNG");
 
     }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -9,7 +9,10 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +21,11 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
 
@@ -25,17 +33,71 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     LocationRequest request;
     double lat, lng;
     TextView textView;
-
+    Button next;
+    String userId;
+    String name;
+    String mobnum;
+    EditText nt,et;
+    public String msg;
+    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("users");
+   // private DatabaseReference mDatabase= FirebaseDatabase.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        userId = mDatabase.push().getKey();
         setContentView(R.layout.activity_main);
+        nt=(EditText) findViewById(R.id.name);
+        et=(EditText) findViewById(R.id.mobnum);
+
         textView=(TextView) findViewById(R.id.location);
         buildApiClient();
         apiClient.connect();
 
+
     }
 
+    /*public void receiveData(View view) {
+        mDatabase.child(userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                User user = dataSnapshot.getValue(User.class);
+                //Toast.makeText(getApplicationContext(),""+user.getName()+user.getMsg(),Toast.LENGTH_LONG);
+                et.setText(user.getMsg());
+                //Log.d(TAG, "User name: " + user.getName() + ", email " + user.getMsg());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                // Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }*/
+
+    public static class User {
+
+        public String name;
+        public String mobnum;
+
+        public User() {
+            // Default constructor required for calls to DataSnapshot.getValue(User.class)
+        }
+
+        public User(String name, String mobnum) {
+            this.name = name;
+            this.mobnum= mobnum;
+        }
+        public String getName()
+        {
+            return  this.name;
+        }
+        public String getMobnum()
+        {
+            return this.mobnum;
+        }
+
+    }
     protected synchronized void buildApiClient() {
         apiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
@@ -83,14 +145,27 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         if (location != null) {
             lat = location.getLatitude();
             lng = location.getLongitude();
-            String msg = "lat: " + lat + ",lng: " + lng;
+            msg = "lat: " + lat + ",lng: " + lng;
             //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+            name=nt.getText()+"";
+            mobnum=et.getText()+"";
+            if(name!="")
+            {
+            User user = new User(name,mobnum);
+            mDatabase.child(userId).setValue(user);//storing to data base
             textView.setText(msg);
+            }
         } else {
             Toast.makeText(this, "Location is null", Toast.LENGTH_SHORT).show();
+
         }
 
     }
+   /* private void writeNewUser(String userId, String name, String msg) {
+        User user = new User(name, msg);
+
+        mDatabase.child("users").child(userId).setValue(user);
+    }*/
 
     public void get(View v) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -108,15 +183,19 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         if (location!=null){
             lat=location.getLatitude();
             lng=location.getLongitude();
-            String msg="Lat : "+lat+" , Lng : "+lng;
+            msg="Lat : "+lat+" , Lng : "+lng;
             //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+            User user = new User(name,mobnum);
+            mDatabase.child(userId).setValue(user);
+            Toast.makeText(this,msg+" "+userId,Toast.LENGTH_SHORT);
             textView.setText(msg);
         }
         else {
             Toast.makeText(this, "Location is Null", Toast.LENGTH_SHORT).show();
         }
     }
-    public void showMap(View view){
+    public void showMap(View v){
 
         Intent i=new Intent(this,GoogleMapsActivity.class);
         i.putExtra("LAT",lat);
